efefe9dd017a3ef12910dfec85e76a6279ef7e2f
BelChaparro <a.belenchaparro@gmail.com>
2024-05-09 16:04:29 -0300

    implemented updates up until chapter 12 of the tutorial

---
+++ app/lib/actions.ts
@@ -0,0 +1,59 @@
+"use server"; // All exported functions within the file are now server functions.
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+// Validate data types with zod:
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100; // Convert amount into cents to eliminate JS floating-point errors and ensure greater accuracy
+    const date = new Date().toISOString().split('T')[0]; // Create invoice's creation date with "YYY-MM-DD" format
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+    revalidatePath('/dashboard/invoices'); // Clear cache and trigger new request to the server
+    redirect('/dashboard/invoices'); // Redirect the user back to the /dashboard/invoices page
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100; // Convert amount into cents to eliminate JS floating-point errors and ensure greater accuracy
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices'); // Clear cache and trigger new request to the server
+    redirect('/dashboard/invoices'); // Redirect the user back to the /dashboard/invoices page
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices"); // Clear cache and trigger new request to the server
+}
